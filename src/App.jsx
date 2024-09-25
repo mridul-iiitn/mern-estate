7a2b4c5717f9521a7c01cc5efcdf6de2f9bb8a43
Mridul <mridulpandey619@gmail.com>
2024-09-25 15:09:11 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React, { PureComponent } from 'react'
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signout from "./pages/Signup";
+import Profile from "./pages/Profile";
 
-export default class App extends PureComponent {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+function App() {
+  return <BrowserRouter>
+  <Routes>  
+    <Route path="/" element={<Home/>}/>
+    <Route path="/sign-in" element={<Signin/>}/>
+    <Route path="/sign-out" element={<Signup/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+
+
+  </Routes>
+  </BrowserRouter>
 }
- 
+
+export default App
